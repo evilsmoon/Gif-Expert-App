9384102c2be49bd55e5c22d116a16587c01d46be
Gabriel Morales <gabrielmorales201564@hotmail.com>
2020-09-16 14:41:32 -0400

    change name build to docs

---
+++ docs/precache-manifest.884c6c27fe5b031b1ff7d59d055a63fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f338ec7cdb1d8a90c5519ef96ff4a043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec35eb4f2b9493edf96",
+    "url": "/static/css/main.f9dbca75.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec35eb4f2b9493edf96",
+    "url": "/static/js/main.d9bbcea5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
